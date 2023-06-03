998f3b2af5b4044ed8c53f9cdb1ccbf501fc19d9
Michael Teuscher <michael.teuscher@pm.me>
2023-06-03 11:03:44 +0200

    filter out more useless log messages

--- pkg/modules/mojitolog/filter.go
+++ pkg/modules/mojitolog/filter.go
@@ -11,9 +11,15 @@ func filter(msg map[string]interface{}) bool {
 		return true
 	}
 
-	// Hide misleading caddy warning, this is intentional and not a bug
 	if val, ok := msg["server_name"]; ok && val == "https_server" {
+		// Hide misleading caddy warning, this is intentional and not a bug
 		if val, ok := msg["msg"]; ok && val == "automatic HTTP->HTTPS redirects are disabled" {
+			return true
+		}
+
+		// Hide spammy message about skipping automatic cert management
+		// E.g: skipping automatic certificate management because one or more matching certificates are already loaded
+		if val, ok := msg["msg"]; ok && strings.Contains(fmt.Sprint(val), "skipping automatic certificate management") {
 			return true
 		}
 	}
@@ -31,6 +37,13 @@ func filter(msg map[string]interface{}) bool {
 
 	if val, ok := msg["logger"]; ok && val == "tls" {
 		if val, ok := msg["error"]; ok && strings.Contains(fmt.Sprint(val), "cloudflare origin certificate") {
+			return true
+		}
+	}
+
+	// Disable "enable automatic TLS certificate management" since domain list can get long
+	if val, ok := msg["logger"]; ok && val == "http" {
+		if val, ok := msg["msg"]; ok && strings.Contains(fmt.Sprint(val), "enabling automatic TLS certificate management") {
 			return true
 		}
 	}
